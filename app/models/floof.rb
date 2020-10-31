80e6888f90061405501ac847a6f0d1396a3f44e6
CJ Gilroy <cjgilroy@MacBook-Pro.local>
2020-10-31 16:11:34 -0600

    Add color inclusion validation

--- app/models/floof.rb
+++ app/models/floof.rb
@@ -4,7 +4,7 @@ class Floof < ApplicationRecord
     include ActionView::Helpers::DateHelper
     validates :sex, length: { maximum: 1 }
     validates :name, :birth_date, :sex, :color, :description, presence:true
-
+    validates :color, inclusion: { in: %w(cream black white grey orange) }
     def age
         time_ago_in_words(birth_date)
     end
