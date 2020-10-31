dae116235c206dc0aa1e86067f5b855149853b18
CJ Gilroy <cjgilroy@MacBook-Pro.local>
2020-10-31 16:04:50 -0600

    Add #age method to Floof class

--- app/models/floof.rb
+++ app/models/floof.rb
@@ -1,4 +1,11 @@
+require "action_view"
+
 class Floof < ApplicationRecord
+    include ActionView::Helpers::DateHelper
     validates :sex, length: { maximum: 1 }
     validates :name, :birth_date, :sex, :color, :description, presence:true
+
+    def age
+        time_ago_in_words(birth_date)
+    end
 end
