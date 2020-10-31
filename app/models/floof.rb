45af920fbe5e2b2fe54458ccb39d57dac22f434d
CJ Gilroy <cjgilroy@MacBook-Pro.local>
2020-10-31 16:15:41 -0600

    Validate M/F sex value

--- app/models/floof.rb
+++ app/models/floof.rb
@@ -2,7 +2,7 @@ require "action_view"
 
 class Floof < ApplicationRecord
     include ActionView::Helpers::DateHelper
-    validates :sex, length: { maximum: 1 }
+    validates :sex, inclusion: { in: %w(M F) }
     validates :name, :birth_date, :sex, :color, :description, presence:true
     validates :color, inclusion: { in: %w(cream black white grey orange) }
     def age
