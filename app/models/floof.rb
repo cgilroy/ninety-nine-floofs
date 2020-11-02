85648844e72e46a3bc749930c4e1968657f88748
CJ Gilroy <cjgilroy@MacBook-Pro.local>
2020-11-01 18:38:01 -0700

    Add color to form

--- app/models/floof.rb
+++ app/models/floof.rb
@@ -2,9 +2,10 @@ require "action_view"
 
 class Floof < ApplicationRecord
     include ActionView::Helpers::DateHelper
+    COLORS = %w(cream black white grey orange)
     validates :sex, inclusion: { in: %w(M F) }
     validates :name, :birth_date, :sex, :color, :description, presence:true
-    validates :color, inclusion: { in: %w(cream black white grey orange) }
+    validates :color, inclusion: { in: COLORS }
     def age
         time_ago_in_words(birth_date)
     end
