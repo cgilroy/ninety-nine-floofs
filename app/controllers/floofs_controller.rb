7255c13fea60b277a47feaa727e61827f9d847db
CJ Gilroy <cjgilroy@MacBook-Pro.local>
2020-11-02 17:18:34 -0700

    Add route to create floof

--- app/controllers/floofs_controller.rb
+++ app/controllers/floofs_controller.rb
@@ -8,4 +8,24 @@ class FloofsController < ApplicationController
         @floof = Floof.find(params[:id])
         render :show
     end
+
+    def new
+        @floof = Floof.new
+        render :new
+    end
+
+    def create
+        @floof = Floof.new(floof_params)
+        if @floof.save
+            redirect_to floof_url(@floof)
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def floof_params
+        params.require(:floof).permit(:color,:description,:name,:birth_date,:sex)
+    end
 end
