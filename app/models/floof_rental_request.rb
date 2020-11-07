0622fabc378a1ff8141961675b4d3e67e9da267c
CJ Gilroy <cjgilroy@MacBook-Pro.local>
2020-11-07 13:53:20 -0700

    Validate that request doesn't overlap existing req

--- app/models/floof_rental_request.rb
+++ app/models/floof_rental_request.rb
@@ -1,17 +1,30 @@
 class FloofRentalRequest < ApplicationRecord
     validates :floof_id, :start_date, :end_date, :status, presence: true
     validates :status, inclusion: %w(PENDING APPROVED DENIED)
+    validate :does_not_overlap_approved_request
     
 
     belongs_to :floof
 
 
     
-
+    private
+    
     def overlapping_requests
         FloofRentalRequest.where.not(id: self.id)
         .where(floof_id: self.floof_id)
         .where.not('start_date > :end_date OR end_date < :start_date',
             start_date: self.start_date, end_date: self.end_date)
+    end
+
+    def overlapping_approved_requests
+        overlapping_requests.where(status: 'APPROVED')
+    end
+
+    def does_not_overlap_approved_request
+        return if self.status == 'DENIED'
+        if overlapping_approved_requests.exists?
+            errors[:base] << 'Request overlaps with existing rental'
+        end
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ f1 = Floof.create(:name=>'Ollie',:birth_date=>'2015-04-15',:color=>'cream',:sex=
 f2 = Floof.create(:name=>'Riley',:birth_date=>'2017-09-17',:color=>'white',:sex=>'F',:description=>'Riley pup!')
 f3 = Floof.create(:name=>'Bear',:birth_date=>'2019-08-01',:color=>'brown',:sex=>'M',:description=>'Little bear boy!')
 
-req_1 = FloofRentalRequest.create(:floof_id=>f1.id,:start_date=>'2020-11-10',:end_date=>'2020-11-14')
+req_1 = FloofRentalRequest.create(:floof_id=>f1.id,:start_date=>'2020-11-10',:end_date=>'2020-11-14',:status=>'APPROVED')
 req_2 = FloofRentalRequest.create(:floof_id=>f1.id,:start_date=>'2020-11-05',:end_date=>'2020-11-13')
 req_3 = FloofRentalRequest.create(:floof_id=>f1.id,:start_date=>'2020-11-10',:end_date=>'2020-11-21')
-req_4 = FloofRentalRequest.create(:floof_id=>f1.id,:start_date=>'2020-11-05',:end_date=>'2020-11-21')
+req_4 = FloofRentalRequest.create(:floof_id=>f1.id,:start_date=>'2020-11-05',:end_date=>'2020-11-21',:status=>'APPROVED')
