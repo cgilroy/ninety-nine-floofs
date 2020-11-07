9d0c530a6ce075faa89ab691e751eff3509b52a8
CJ Gilroy <cjgilroy@MacBook-Pro.local>
2020-11-07 12:16:57 -0700

    Add association between floof and rental request

--- app/models/floof.rb
+++ app/models/floof.rb
@@ -2,10 +2,17 @@ require "action_view"
 
 class Floof < ApplicationRecord
     include ActionView::Helpers::DateHelper
-    COLORS = %w(cream black white grey orange)
+    COLORS = %w(cream black white grey orange brown)
     validates :sex, inclusion: { in: %w(M F) }
     validates :name, :birth_date, :sex, :color, :description, presence:true
     validates :color, inclusion: { in: COLORS }
+
+    has_many :rental_requests, 
+        foreign_key: :floof_id,
+        primary_key: :id,
+        class_name: :FloofRentalRequest,
+        dependent: :destroy
+
     def age
         time_ago_in_words(birth_date)
     end
--- app/models/floof_rental_request.rb
+++ app/models/floof_rental_request.rb
@@ -2,4 +2,6 @@ class FloofRentalRequest < ApplicationRecord
     validates :floof_id, :start_date, :end_date, :status, presence: true
     validates :status, inclusion: %w(PENDING APPROVED DENIED)
     
+
+    belongs_to :floof
 end
