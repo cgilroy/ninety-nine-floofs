7e061b123efa96811e1a80664a2bc90b5c55f1cb
CJ Gilroy <c.gilroy9@gmail.com>
2020-11-18 14:13:03 -0700

    Add User model with session and password methods

---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+require 'bcrypt'
+class User < ApplicationRecord
+    validates :user_name, :password_digest, presence:true
+
+    attr_reader :password
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def self.generate_session_token
+        SecureRandom::urlsafe_base64(16)
+    end
+
+    def reset_session_token!
+        self.session_token = self.class.generate_session_token
+        self.save!
+        self.session_token
+    end
+end
